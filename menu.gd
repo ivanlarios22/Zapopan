46cc2c063a86346070eea17b64e509f01e9631bf
amyshoto <amydsotom@gmail.com>
2024-11-19 23:48:58 -0600

    Pantalla finalización
    -Escena final del juego

--- menu.gd
+++ menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _on_jugar_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
+	# get_tree().change_scene_to_file("res://final.tscn")
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
